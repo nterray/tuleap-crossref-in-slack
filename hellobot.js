1cef5ea42f7a62e595e8ac10548b34c14559a1d9
Nicolas Terray <nicolas.terray@gmail.com>
2015-06-03 22:45:33 +0200

    extract cross ref whenever possible

--- hellobot.js
+++ hellobot.js
@@ -1,13 +1,31 @@
 module.exports = function (req, res, next) {
   var userName = req.body.user_name;
-  var botPayload = {
-    text : 'Hello, ' + userName + '!'
-  };
-
-  // avoid infinite loop
-  if (userName !== 'slackbot') {
-    return res.status(200).json(botPayload);
-  } else {
+  if (userName === 'slackbot') {
+    // avoid infinite loop
     return res.status(200).end();
   }
+
+  var regex = /(\w+)\s\#([\w-_]+:)?((?:&amp;|\w|\/|&)+)/g;
+  var links = [];
+  while ((match = regex.exec(req.body.text)) !== null) {
+    var server = 'my.enalean.com',
+        key    = match[1],
+        value  = match[3];
+
+    switch (key) {
+        case 'community':
+        case 'request':
+        case 'story':
+            server = 'tuleap.net'
+            break;
+        default:
+            break;
+    }
+    links.push('https://'+ server +'/goto?key='+ key +'&val='+ value);
+  }
+  var message = {
+    text : links.join('\\n')
+  };
+
+  return res.status(200).json(message);
 }

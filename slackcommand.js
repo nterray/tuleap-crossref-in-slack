d19d245a52dee04b5c8d84ff845649644ead9317
Nicolas Terray <nicolas.terray@gmail.com>
2015-06-03 23:49:07 +0200

    Use incoming webhook to post the link to the channel

--- slackcommand.js
+++ slackcommand.js
@@ -1,3 +1,5 @@
+var request = require('request');
+
 module.exports = function (req, res, next) {
   var userName = req.body.user_name;
   if (userName === 'slackbot') {
@@ -6,12 +8,43 @@ module.exports = function (req, res, next) {
   }
 
   if (! /^\d+$/.test(req.body.text)) {
-      return res.status(200).end();
+      return res.status(200).send('<artifact_id>');
   }
 
   var message = {
-      text : 'https://tuleap.net/goto?group_id=101&key=request&val='+ (+req.body.text)
+      text : '<https://tuleap.net/goto?group_id=101&key=request&val='+ (+req.body.text) +'>',
+      username : 'Tuleap CrossRef',
+      channel  : req.body.channel_id,
+      icon_emoji : ':tulip'
   };
 
-  return res.status(200).json(message);
+    send(message, function (error, status, body) {
+        if (error) {
+            return next(error);
+        } else if (status !== 200) {
+            // inform user that our Incoming WebHook failed
+            return next(new Error('Incoming WebHook: ' + status + ' ' + body));
+        } else {
+            return res.status(200).end();
+        }
+    });
+
+}
+
+
+function send (payload, callback) {
+  var path = process.env.INCOMING_WEBHOOK_PATH;
+  var uri = 'https://hooks.slack.com/services' + path;
+ 
+  request({
+    uri: uri,
+    method: 'POST',
+    body: JSON.stringify(payload)
+  }, function (error, response, body) {
+    if (error) {
+      return callback(error);
+    }
+ 
+    callback(null, response.statusCode, body);
+  });
 }
